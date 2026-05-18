d240aa42fe9553929950249bfe13a3578f4fadbb
enojaroff <e.nojaroff@claims-ia.fr>
2026-05-18 04:56:27 +0200

    Added .spectral.mjs
    Committed by edouard.nojaroff@gmail.com on behalf of enojaroff

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMxMjczMQ/spectral.js?branch_slug=main&token=1c0c99b2-6098-4a37-953e-b67157c7bfa3";
+export default { extends: ruleset };
